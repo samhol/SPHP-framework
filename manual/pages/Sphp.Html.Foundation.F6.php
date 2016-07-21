d4ab4b6a4447e34862e67a149de3b8dc5ec9a553
Sami Holck <sami.holck@gmail.com>
2016-07-21 09:59:46 +0300

    Minor fixes

---
+++ manual/pages/Sphp.Html.Foundation.F6.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Sphp\Html\Foundation;
+
+use Sphp\Html\Foundation\F6\Grids\Grid as Grid;
+use Sphp\Html\Foundation\F6\Grids\Row as Row;
+use Sphp\Html\Foundation\F6\Grids\Column as Column;
+
+$grid = $api->classLinker(Grid::class);
+$row = $api->classLinker(Row::class);
+$col = $api->classLinker(Column::class);
+$cols = $api->getClassLink(Column::class, "Columns");
+//$ns = $api->getNamespaceLink(__NAMESPACE__);
+$ns = $api->getNamespaceBreadGrumbs(__NAMESPACE__);
+$f_GridLink = $foundation->getComponentLink(Grid::class, "Foundation Grid layout");
+echo $parsedown->text(<<<MD
+#Foundation front-end framework
+        
+$ns
+
+Foundation framework is included in SPHP and therefore also all of Foundation 
+clientside properties are available. Here is a small collection of features available.
+MD
+);
+$load("Sphp.Html.Foundation-orbit-intro.php");
