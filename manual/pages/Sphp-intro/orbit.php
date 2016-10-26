17592c0e11b05c010c004143d6c5d88e236a7950
Sami Holck <sami.holck@gmail.com>
2016-10-26 09:51:27 +0300

    Manual update

--- manual/pages/Sphp-intro/orbit.php
+++ manual/pages/Sphp-intro/orbit.php
@@ -11,7 +11,7 @@ $orbitIntro->setAnimIn("fade-in")->setAnimOut("fade-out")->pauseOnHover();
 $orbitIntro->addCssClass("intro");
 $orbitIntro->appendMdFile($path->local("manual/pages/Sphp-intro/introduction.php"));
 $orbitIntro->appendMdFile($path->local("manual/pages/Sphp-intro/libraries.php"));
-$orbitIntro->appendMdFile($path->local("manual/pages/Core-intro/ErrorHandling.php"));
+//$orbitIntro->appendMdFile($path->local("manual/pages/Core-intro/ErrorHandling.php"));
 $orbitIntro->appendMdFile($path->local("manual/pages/Foundation-intro/Grids.php"));
 $orbitIntro->appendMdFile($path->local("manual/pages/Foundation-intro/Media.php"));
 $orbitIntro->appendMdFile($path->local("manual/pages/Foundation-intro/Forms.php"));
