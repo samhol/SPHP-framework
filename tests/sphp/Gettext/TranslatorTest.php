d831294ec42cdcd259b8c611663bbf56577842da
Sami Holck <sami.holck@gmail.com>
2016-11-22 15:29:12 +0200

    Updates

--- tests/sphp/Gettext/TranslatorTest.php
+++ tests/sphp/Gettext/TranslatorTest.php
@@ -2,6 +2,7 @@
 
 namespace Sphp\Core\Gettext;
 
+use Zend\I18n\Translator\Translator as ZendTranslator;
 /**
  */
 class TranslatorTest extends \PHPUnit_Framework_TestCase {
@@ -17,11 +18,17 @@ class TranslatorTest extends \PHPUnit_Framework_TestCase {
    */
   protected function setUp() {
     //$this->setLocale("fi_FI");
-    $this->translator = new Translator('Finnish_Finland');
-    setlocale(LC_ALL, 'Finnish_Finland');
-    putenv('LC_ALL=Finnish_Finland');
-    var_dump(getenv ('LC_ALL'));
-    var_dump(setLocale(LC_ALL, "0"));
+    bindtextdomain(\Sphp\DEFAULT_DOMAIN, \Sphp\LOCALE_PATH);
+    bind_textdomain_codeset(\Sphp\DEFAULT_DOMAIN, 'UTF-8');
+    $this->translator = new Translator('fi_FI');
+    //putenv('LC_ALL=fi_FI');
+    putenv('LC_ALL=fi_FI');
+    //setlocale(\LC_ALL, 'fi_FI.utf8');
+    //setlocale(\LC_ALL, ""); LC_CTYPE
+    //setlocale(\LC_ALL, 'fi_FI.utf8');
+    var_dump(getenv('LC_ALL'));
+    var_dump(setLocale(\LC_ALL, "0"));
+    //$locale = \Locale::acceptFromHttp($_SERVER['HTTP_ACCEPT_LANGUAGE']);
   }
 
   protected function setLocale($locale) {
@@ -39,9 +46,13 @@ class TranslatorTest extends \PHPUnit_Framework_TestCase {
   /**
    */
   public function testSingular() {
+    $zendTranslator = new ZendTranslator();
+    $zendTranslator->addTranslationFilePattern('gettext', 'D:\web_projects\SPHP-framework\sphp\locale', "%s\LC_MESSAGES\Sphp.Defaults.mo", 'Sphp.Defaults');
+   var_dump($zendTranslator->translate("year", \Sphp\DEFAULT_DOMAIN, 'fi_FI'));
     var_dump($this->translator->get("year"));
-    $this->assertEquals($this->translator->get("year"), "vuosi");
-    $this->assertEquals($this->translator->get("month", "en_US"), "month");
+    var_dump(\Sphp\LOCALE_PATH.'\%s\LC_MESSAGES\Sphp.Defaults.mo');
+    //$this->assertEquals($this->translator->get("year"), "vuosi");
+    //$this->assertEquals($this->translator->get("month", "en_US"), "month");
   }
 
 }
