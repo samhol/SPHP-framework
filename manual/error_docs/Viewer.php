02a7348e2983def9a971c7612bd22fe759e445d0
Sami Holck <sami.holck@gmail.com>
2016-12-21 16:45:39 +0200

    Updates

--- manual/error_docs/Viewer.php
+++ manual/error_docs/Viewer.php
@@ -43,6 +43,7 @@ class Viewer implements ContentInterface {
     }
     $cont->appendMd('* [SPHP framework](http://playground.samiholck.com/){.error}');
     $cont->appendMd('* [SPHP framework API](http://documentation.samiholck.com/apigen/){.error}');
+    $cont->appendMd('* Technical Contact: <a href="mailto:sami.holck@samiholck.com">sami.holck@samiholck.com</a>');
     $cont->append('<div class="row columns"><div class="http-code float-right">' . $this->code->getCode() . '</div></div>');
     $path = Path::get()->http('manual/pics/error.png');
     $s25 = new Size(25, 25);
