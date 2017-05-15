1e61c537db0ec54a71e9e29b73d91939cc19959f
Sami Holck <sami.holck@gmail.com>
2017-05-15 12:55:33 +0300

    Updates

--- manual/templates/logo-area.php
+++ manual/templates/logo-area.php
@@ -3,10 +3,11 @@
 namespace Sphp\Html\Foundation\Sites\Navigation;
 
 use Sphp\Stdlib\Path;
+
 ?>
 <div class="row expanded sphp-logo-area">
-  <div class="column small-12 large-6">
-    <ul class="logo menu">
+  <div class="column small-12 medium-6">
+    <ul class="logo">
       <li>
         <a href="<?php echo Path::get()->http() ?>" target="_self" title="Navigate back to frontpage" data-sphp-qtip>
           <img src="manual/pics/sphp-code-logo.png" alt="SPHP framework">
@@ -14,19 +15,20 @@ use Sphp\Stdlib\Path;
       </li>
     </ul>
   </div>
-  <div class="column small-12 large-6 text-right">
+  <div class="column small-12 medium-6 icon-col">
     <?php
 
+    use Sphp\Html\Icons\BrandIcons;
 
-            use Sphp\Html\Icons\BrandIcons;
-            $bi = new BrandIcons();
-            
-            $bi->setGithub('https://github.com/samhol/SPHP-framework');
-            $bi->setFacebook('https://www.facebook.com/Sami.Petteri.Holck.Programming/');
-            $bi->setGooglePlus('https://plus.google.com/b/113942361282002156141/113942361282002156141');
-            $bi->setTwitter('https://twitter.com/SPHPframework');
-            $bi->addCssClass('rounded');
-            $bi->printHtml();
+$bi = new BrandIcons();
+
+    $bi->setGithub('https://github.com/samhol/SPHP-framework');
+    $bi->setFacebook('https://www.facebook.com/Sami.Petteri.Holck.Programming/');
+    $bi->setGooglePlus('https://plus.google.com/b/113942361282002156141/113942361282002156141');
+    $bi->setTwitter('https://twitter.com/SPHPframework');
+    $bi->addCssClass(['rounded', 'logo']);
+    $bi->printHtml();
+
     ?>
   </div>
 </div>
