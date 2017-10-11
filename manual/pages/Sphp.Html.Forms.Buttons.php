cfb66873516e94d4a1d1cdec79e2304f53554451
Sami Holck <sami.holck@gmail.com>
2017-10-11 19:10:47 +0300

    Update

--- manual/pages/Sphp.Html.Forms.Buttons.php
+++ manual/pages/Sphp.Html.Forms.Buttons.php
@@ -4,6 +4,7 @@ namespace Sphp\Html\Forms\Buttons;
 
 use Sphp\Html\Apps\Syntaxhighlighting\CodeExampleBuilder;
 use Sphp\Html\Apps\Manual\Apis;
+
 $submitter = Apis::sami()->classLinker(Submitter::class);
 $reseter = Apis::sami()->classLinker(Resetter::class);
 $button = Apis::sami()->classLinker(Button::class);
@@ -13,6 +14,7 @@ $button = Apis::sami()->classLinker(Button::class);
 * $submitter is a submit button (submits form-data)
 * $reseter is a reset button (resets the form-data to its initial values)
 * $button is is a clickable button
+        
 MD
 );
 CodeExampleBuilder::build('Sphp/Html/Forms/Buttons/AbstractButton.php', 'html5')
