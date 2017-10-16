9887a3cdfd08b2658e3c5c6ae9e9969db92ef2b7
Sami Holck <sami.holck@gmail.com>
2017-10-16 09:29:12 +0300

    Manual Update

--- manual/pages/Sphp.Stdlib.Types.Arrays.php
+++ manual/pages/Sphp.Stdlib.Types.Arrays.php
@@ -3,34 +3,32 @@
 namespace Sphp\Stdlib;
 
 use Sphp\Html\Apps\Syntaxhighlighting\CodeExampleBuilder;
-use Sphp\Html\Apps\Manual\Apis;
-$arraysClass = Apis::sami()->classLinker(Arrays::class);
-$arrLink = Apis::phpManual()->typeLink('array');
-\Sphp\Manual\parseDown(<<<MD
+use Sphp\Manual as Man;
+
+$arraysClass = Man\api()->classLinker(Arrays::class);
+$arrLink = Man\php()->typeLink('array');
+Man\parseDown(<<<MD
 ##The $arraysClass class for PHP's $arrLink manipulation
 
-PHP's $arrLink type is optimized for several different uses; it can be treated as 
+PHP's $arrLink type can be treated as 
 an array, list (vector), hash table (an implementation of a map), dictionary, 
-collection, stack, queue, and much more. As $arrLink values can be other arrays, 
+collection, stack, queue, and much more. As values can be other arrays, 
 trees and multidimensional arrays are also possible.
 
-PHP core itself has several functions for $arrLink manipulation and interaction. $arraysClass 
-is just a small static utility class for some additional $arrLink related operations.
-		
-$arraysClass methods for testing differert $arrLink properties.
+$arraysClass is a static utility class extending $arrLink related operations in 
+PHP core and it introduces methods for:
+
+* testing $arrLink properties.
+* manipulating array properties and creating new arrays.
+* 'cloning' multidimensional PHP arrays.
+
 MD
 );
 
 CodeExampleBuilder::visualize('Sphp/Stdlib/Types/Arrays1.php', 'text', false);
-\Sphp\Manual\parseDown(<<<MD
-$arraysClass methods for manipulating array properties and creating new arrays.
-MD
-);
+
 CodeExampleBuilder::visualize('Sphp/Stdlib/Types/Arrays2.php', 'text', false);
-\Sphp\Manual\parseDown(<<<MD
-$arraysClass class has a method for 'cloning' multidimensional PHP arrays. {$arraysClass->methodLink("copy")} 
-tries to make an independent copy out of each key => value pairs it the input 
-array and it uses PHP's object cloning construct for object type.
-MD
-);
-CodeExampleBuilder::visualize('Sphp/Stdlib/Types/Arrays3.php', 'text', false);
+
+CodeExampleBuilder::build('Sphp/Stdlib/Types/Arrays3.php', 'text', false)
+        ->setExamplePaneTitle('Cloning the content of an array')
+        ->printHtml();
