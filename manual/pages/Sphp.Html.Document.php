944d68104c39a0d12ea616dc184f0b953b9848db
Sami Holck <sami.holck@gmail.com>
2017-10-18 19:45:40 +0300

    Update

--- manual/pages/Sphp.Html.Document.php
+++ manual/pages/Sphp.Html.Document.php
@@ -4,17 +4,17 @@ namespace Sphp\Html;
 
 use Sphp\Html\Apps\Syntaxhighlighting\CodeExampleBuilder;
 use Sphp\Html\Apps\Manual\Apis;
-
-$documentClass = Apis::sami()->classLinker(Document::class);
-$htmlClass = Apis::sami()->classLinker(Html::class);
-\Sphp\Manual\parseDown(<<<MD
+use Sphp\Manual;
+$documentClass = Manual\api()->classLinker(Document::class);
+$htmlClass = Manual\api()->classLinker(Html::class);
+Manual\parseDown(<<<MD
 ##The $documentClass class
 This class can be used to create the structure of any HTML document.
         
 MD
 );
 CodeExampleBuilder::visualize('Sphp/Html/Document.php', "html5", false);
-\Sphp\Manual\parseDown(<<<MD
+Manual\parseDown(<<<MD
 The $documentClass class acts as a factory for basic HTML objects. Here are the 
 grouped lists of the HTML5 components and the corresponding PHP types in Framework's HTML implementation.
 MD
