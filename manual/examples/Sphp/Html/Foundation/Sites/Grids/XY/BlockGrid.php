4ddc72b02afed6d5b8cbf645e0a766e4ed721f62
Sami Holck <sami.holck@gmail.com>
2017-11-09 16:13:55 +0200

    Update

--- manual/examples/Sphp/Html/Foundation/Sites/Grids/XY/BlockGrid.php
+++ manual/examples/Sphp/Html/Foundation/Sites/Grids/XY/BlockGrid.php
@@ -14,7 +14,7 @@ $img30 = (new Img("http://placehold.it/30x30/c0c0c0/333?text=foo", '30x30 foo'))
         ->addCssClass('thumbnail')
         ->setLazy(true);
 
-(new BlockGrid(['small-up-1', 'medium-up-2', 'xlarge-up-4'], [$img200, $img200, $img200, $img200]))
+(new BlockGrid('small-up-1', 'medium-up-2', 'xlarge-up-4'))->setColumns([$img200, $img200, $img200, $img200])
         ->printHtml();
 (new BlockGrid(['small-up-2', 'large-up-4'], [$img100, $img100, $img100, $img100]))
         ->printHtml();
