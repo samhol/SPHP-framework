ac706278a25aab55d62f17b80a186bd8c9523d2b
samhol <sami.holck@gmail.com>
2017-11-15 17:12:17 +0200

    Update

--- manual/ajax/submitRow.php
+++ manual/ajax/submitRow.php
@@ -3,7 +3,7 @@
 namespace Sphp\Html\Foundation\Sites;
 
 error_reporting(E_ALL);
-ini_set('display_errors', '1');
+ini_set('display_errors', 1);
 require_once('../settings.php');
 
 use Sphp\Html\Foundation\Sites\Grids\Column;
