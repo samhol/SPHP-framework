80806a3814a034e55d205d9ade2a4ae5aa0ece72
Sami Holck <sami.holck@gmail.com>
2017-12-21 09:29:44 +0200

    SCSS updates

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 namespace Sphp\Html\Foundation\Sites\Navigation;
 
 require_once('manual/settings.php');
-//ob_implicit_flush(true);
 
 $redirect = filter_input(INPUT_SERVER, 'REDIRECT_URL', FILTER_SANITIZE_URL);
 
@@ -27,8 +26,8 @@ if ($outputCache->start("$cacheSuffix-head") === false) {
     }
     ?>
     <div class="grid-container fluid">
-      <div class="grid-x grid-margin-x grid-padding-x">
-        <div class="cell small-12 medium-3 large-3 xlarge-2 show-for-large">
+      <div class="grid-x grid-margin-x grid-padding-x medium-margin-uncollapse">
+        <div class="cell small-12 large-3 xlarge-3 show-for-large">
           <?php
           if ($outputCache->start('sidenav') === false) {
             include('manual/templates/menus/sidenav.php');
@@ -36,7 +35,7 @@ if ($outputCache->start("$cacheSuffix-head") === false) {
           }
           ?>
         </div>
-        <div class="mainContent  small-12 large-9 xlarge-9 cell"> 
+        <div class="mainContent  small-auto large-9 xlarge-9 cell"> 
           <div class="container">
             <?php
             $man_cache = "$cacheSuffix-content";
@@ -47,18 +46,16 @@ if ($outputCache->start("$cacheSuffix-head") === false) {
             ?>
           </div>
         </div>
-        <div class="show-for-xlarge xlarge-1 cell"></div>
       </div>
     </div>
   </div>
-
-<?php
-if ($outputCache->start('footer') === false) {
-  include('manual/templates/blocks/footer.php');
-  include('manual/templates/backToTopButton.php');
-  $outputCache->end();
-}
-?>
+  <?php
+  if ($outputCache->start('footer') === false) {
+    include('manual/templates/blocks/footer.php');
+    include('manual/templates/backToTopButton.php');
+    $outputCache->end();
+  }
+  ?>
 </div>
 <?php
 $html->documentClose();
