e1bd1d03b11668f6a07c45781a54d52de02ab107
Sami Holck <sami.holck@gmail.com>
2018-02-28 01:52:02 +0200

    Updates

--- index.php
+++ index.php
@@ -13,50 +13,43 @@ if ($outputCache->start("$cacheSuffix-head") === false) {
   $outputCache->end();
 }
 ?>
-<div class="off-canvas-wrapper">
-  <div class="off-canvas position-left" id="bodyOffCanvas" data-off-canvas>
-    <!-- Your menu or Off-canvas content goes here -->
-  </div>
-  <div class="off-canvas-content" data-off-canvas-content>
-    <?php
-    if ($outputCache->start("$cacheSuffix-topbar") === false) {
-      include('manual/templates/logo-area.php');
-      include('manual/templates/menus/topBar.php');
-      $outputCache->end();
-    }
-    ?>
-    <div class="grid-container fluid">
-      <div class="grid-x grid-margin-x grid-padding-x medium-margin-uncollapse">
-        <div class="cell small-12 large-3 xlarge-3 show-for-large">
-          <?php
-          if ($outputCache->start('sidenav') === false) {
-            include('manual/templates/menus/sidenav.php');
-            $outputCache->end();
-          }
-          ?>
-        </div>
-        <div class="mainContent  small-auto large-9 xlarge-9 cell"> 
-          <div class="container">
-            <?php
-            $man_cache = "$cacheSuffix-content";
-            if ($outputCache->start($man_cache) === false) {
-              $router->execute();
-              $outputCache->end();
-            }
-            ?>
-          </div>
-        </div>
+
+<?php
+if ($outputCache->start("$cacheSuffix-topbar") === false) {
+  include('manual/templates/logo-area.php');
+  include('manual/templates/menus/topBar.php');
+  $outputCache->end();
+}
+?>
+<div class="grid-container fluid">
+  <div class="grid-x grid-margin-x grid-padding-x medium-margin-uncollapse">
+    <div class="cell small-12 large-3 xlarge-3 show-for-large">
+      <?php
+      if ($outputCache->start('sidenav') === false) {
+        include('manual/templates/menus/sidenav.php');
+        $outputCache->end();
+      }
+      ?>
+    </div>
+    <div class="mainContent  small-auto large-9 xlarge-9 cell"> 
+      <div class="container">
+        <?php
+        $man_cache = "$cacheSuffix-content";
+        if ($outputCache->start($man_cache) === false) {
+          $router->execute();
+          $outputCache->end();
+        }
+        ?>
       </div>
     </div>
   </div>
-  <?php
-  if ($outputCache->start('footer') === false) {
-    include('manual/templates/blocks/footer.php');
-    include('manual/templates/backToTopButton.php');
-    $outputCache->end();
-  }
-  ?>
 </div>
 <?php
+if ($outputCache->start('footer') === false) {
+  include('manual/templates/blocks/footer.php');
+  include('manual/templates/backToTopButton.php');
+  $outputCache->end();
+}
+
 $html->documentClose();
 
