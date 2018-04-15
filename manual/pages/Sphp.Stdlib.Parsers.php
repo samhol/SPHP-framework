5c5ef4c09701b8c114363df27147760f97632d7e
samhol <sami.holck@gmail.com>
2018-04-15 14:57:11 +0300

    Updates

--- manual/pages/Sphp.Stdlib.Parsers.php
+++ manual/pages/Sphp.Stdlib.Parsers.php
@@ -4,7 +4,6 @@ namespace Sphp\Stdlib\Parsers;
 
 use Sphp\Manual;
 use Sphp\Stdlib\Parser;
-use Sphp\Html\Foundation\Sites\Buttons\ButtonGroup;
 
 $ns = Manual\api()->namespaceBreadGrumbs(__NAMESPACE__);
 $phpArray = Manual\php()->typeLink('array');
@@ -15,29 +14,7 @@ $ns
 $parser instance can handle file related (reading and writing) operations.
 MD
 );
-$yaml = Manual\api()->classLinker(Yaml::class);
-Manual\md(<<<MD
-##YAML <small>data serialization standard</small>
-        
-[YAML](http://yaml.org/) is a human friendly data serialization standard for all programming languages. 
-        
-A instance of $yaml class is able to transform YAML files and strings to PHP arrays and PHP arrays back to YAML files and strings.
-        
-MD
-);
 
-Manual\example('Sphp/Stdlib/Parsers/Yaml.php', null, false)->setExamplePaneTitle('Examples of YAML data manipulation')->printHtml();
-
-$yamlCode = Manual\codeModal('<i class="fas fa-cogs"></i> YAML file', 'manual/snippets/example.yml', 'YAML file example');
-$yamlCodeButton = $yamlCode->getTrigger()->addCssClass('button', 'alert', 'radius', 'small');
-$parsedYaml = Manual\codeModalFromString('<i class="fab fa-php"></i> PHP array', print_r(Parser::fromFile('manual/snippets/example.yml'), true), 'php', 'YAML-file as parsed PHP array');
-$tr1 = $parsedYaml->getTrigger()->addCssClass('button', 'php', 'radius', 'small');
-
-
-$buttonGroup = new ButtonGroup();
-$buttonGroup->setSize('small');
-$buttonGroup->appendButton($yamlCode->getTrigger());
-$buttonGroup->appendButton($parsedYaml->getTrigger());
-echo $buttonGroup . $yamlCode->getPopup() . $parsedYaml->getPopup();
-
+Manual\loadPage('Sphp.Stdlib.Parsers.Yaml');
+Manual\loadPage('Sphp.Stdlib.Parsers.Json');
 Manual\loadPage('Sphp.Stdlib.Parsers.Markdown');
