9ee6d53bdf9411bb7d4434e4aa237c4051d30836
Sami Holck <sami.holck@gmail.com>
2018-04-18 17:53:42 +0300

    Update

--- manual/pages/c.php
+++ manual/pages/c.php
@@ -23,8 +23,12 @@ $holidayData = [
         9 => [
             16 => ['My Birthday']
         ],
+        11 => [
+            '2 Sunday' => 'Fd'
+        ],
         12 => [
             6 => ['Independence Day'],
+            23 => ['Christmas Eve', "Ella's birthday"],
             24 => ['Christmas Eve', 'Birthday'],
             25 => ['Christmas Day'],
             26 => ['Boxing Day'],
@@ -42,6 +46,19 @@ print_r($holidays->get(new DateTime('2016-12-24')));
 
 $dtw = new \Sphp\DateTime\DateTimeWrapper();
 
-echo "\n".$dtw->getWeekDay();
-echo "\n".$dtw->getMonth();
-echo "\n".$dtw->getMonthDay();
+echo "\n" . $dtw->getWeekDay();
+echo "\n" . $dtw->getMonth();
+echo "\n" . $dtw->getMonthDay();
+echo "\n" . $dtw->getWeekDayName();
+echo "\n" . $dtw->getMonthName();
+echo "\n" . $dtw->getYear();
+$ip = new DateTime('november 1st');
+
+$ip->modify("next Sunday +7 days");
+
+echo "\n" . $ip->format('Y-m-d l');
+$jp = new DateTime('november 1st');
+
+$jp->modify("next Saturday +7 days");
+
+echo "\n" . $jp->format('Y-m-d l');
