e4b6291d63684bf3688b85b1e36a511d283035d8
Sami Holck <sami.holck@gmail.com>
2018-05-11 01:24:34 +0300

    Updates

--- pdf.php
+++ pdf.php
@@ -5,11 +5,13 @@ $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8',
 
 // set document information
 $pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('Our Code World');
-$pdf->SetTitle('Example Write Html');
+$pdf->SetAuthor('Sami Holck <sami.holck@gmail.com>');
+$pdf->SetTitle('SPHPlayground Manual');
+$pdf->SetSubject('SPHPlayground Tutorial');
+$pdf->SetKeywords('php, html, example, manual, guide');
 
 // set default header data
-$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE . ' 006', PDF_HEADER_STRING);
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, 'SPHPlayground Manual', PDF_HEADER_STRING);
 
 // set header and footer fonts
 $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
@@ -31,7 +33,7 @@ $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
 // add a page
 $pdf->AddPage();
-$html = '<h4>PDF Example</h4><br><p>Welcome to the Jungle</p>';
+$html = '<h4>SPHPlayground Manual</h4><p></p>';
 
 $pdf->writeHTML($html, true, false, true, false, '');
 // add a page
