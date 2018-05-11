2afde4945fc437f05a1d4819c5df2e2acd18d91a
Sami Holck <sami.holck@gmail.com>
2018-05-11 03:04:15 +0300

    Updates

--- pdf.php
+++ pdf.php
@@ -1,51 +1,26 @@
 <?php
 
 require_once('manual/settings.php');
-$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-// set document information
-$pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('Sami Holck <sami.holck@gmail.com>');
-$pdf->SetTitle('SPHPlayground Manual');
-$pdf->SetSubject('SPHPlayground Tutorial');
-$pdf->SetKeywords('php, html, example, manual, guide');
+$mpdf = new \Mpdf\Mpdf();
+$mpdf->SetHTMLHeader('
+<div >
+    <span style="float: left; font-weight: bold;">SPHPlayground manual</span> <span style="float: right; font-weight: bold;">{PAGENO}/{nbpg}</span>
+</div>');
 
-// set default header data
-$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, 'SPHPlayground Manual', PDF_HEADER_STRING);
+$mpdf->SetHTMLFooter('
+<table width="100%" border="0">
+    <tr>
+        <td width="33%" style="border: none;">{DATE j-m-Y}</td>
+        <td width="33%" align="center">{PAGENO}/{nbpg}</td>
+        <td width="33%" style="text-align: right;">My document</td>
+    </tr>
+</table>');
+$mpdf->Bookmark('Start of the document');
 
-// set header and footer fonts
-$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
-$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
-
-// set default monospaced font
-$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
-
-// set margins
-$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-
-// set auto page breaks
-$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-
-// set image scale factor
-$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
-
-// add a page
-$pdf->AddPage();
-$html = '<h4>SPHPlayground Manual</h4><p></p>';
-
-$pdf->writeHTML($html, true, false, true, false, '');
-// add a page
-$pdf->AddPage();
 
 $css = '<style>' . file_get_contents('manual/css/pdf/pdf.css') . "</style>\n";
 $html = $css . '<div class="pdf">' . Sphp\Manual\createPage('Sphp.Html') . '</div>';
-// output the HTML content
-$pdf->writeHTML($html, true, false, true, false, '');
+$mpdf->WriteHTML('<div>Section 1 text</div>' . $html);
 
-// reset pointer to the last page
-$pdf->lastPage();
-//echo $html;
-//Close and output PDF document
-$pdf->Output('example_006.pdf', 'I');
+$mpdf->Output();
