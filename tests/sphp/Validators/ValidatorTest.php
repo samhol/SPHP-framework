a00c2b3510511d1fa2d9fcdc5fab0276fcfd6841
Sami Holck <sami.holck@gmail.com>
2018-10-24 18:29:28 +0300

    Updates

--- tests/sphp/Validators/ValidatorTest.php
+++ tests/sphp/Validators/ValidatorTest.php
@@ -1,9 +1,11 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * SPHPlayground Framework (http://playgound.samiholck.com/)
+ *
+ * @link      https://github.com/samhol/SPHP-framework for the source repository
+ * @copyright Copyright (c) 2007-2018 Sami Holck <sami.holck@gmail.com>
+ * @license   https://opensource.org/licenses/MIT The MIT License
  */
 
 namespace Sphp\Validators;
