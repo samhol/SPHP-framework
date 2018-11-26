4d33ba98634538c0b250f56023dca66b5d69c83f
samhol <sami.holck@gmail.com>
2018-11-26 18:18:51 +0200

    Updates

---
+++ sphp/php/Sphp/DateTime/Duration.php
@@ -0,0 +1,161 @@
+<?php
+
+/**
+ * SPHPlayground Framework (http://playgound.samiholck.com/)
+ *
+ * @link      https://github.com/samhol/SPHP-framework for the source repository
+ * @copyright Copyright (c) 2007-2018 Sami Holck <sami.holck@gmail.com>
+ * @license   https://opensource.org/licenses/MIT The MIT License
+ */
+
+namespace Sphp\DateTime;
+
+use Sphp\Stdlib\Strings;
+
+/**
+ * Implements a date interval
+ * 
+ * @author  Sami Holck <sami.holck@gmail.com>
+ * @license https://opensource.org/licenses/MIT The MIT License
+ * @filesource
+ */
+class Duration {
+
+  private $seconds;
+
+  /**
+   * Constructor
+   * 
+   * @param string $seconds an interval specification.
+   */
+  public function __construct(int $seconds) {
+    $this->seconds = $seconds;
+  }
+
+  /**
+   * Returns the string representation of the object
+   * 
+   * @return string the string representation of the object
+   */
+  public function __toString(): string {
+    $format = 'P';
+    if ($this->y > 0) {
+      $format .= $this->y . 'Y';
+    }
+    if ($this->m > 0) {
+      $format .= $this->m . 'M';
+    }
+    if ($this->d > 0) {
+      $format .= $this->d . 'D';
+    }
+    $time = '';
+    if ($this->h > 0) {
+      $time .= $this->h . 'H';
+    }
+    if ($this->i > 0) {
+      $time .= $this->i . 'M';
+    }
+    if ($this->s > 0) {
+      $time .= $this->s . 'S';
+    }
+    if ($time !== '') {
+      $format .= "T$time";
+    }
+    return $format;
+  }
+
+  public function add(Duration $d) {
+    $this->addSeconds($d->toSeconds());
+    return $this;
+  }
+
+  public function addSeconds(int $seconds) {
+    $this->seconds += $seconds;
+    return $this;
+  }
+
+  public function addMinutes(int $minutes) {
+    $this->seconds += $minutes * 60;
+    return $this;
+  }
+
+  public function addHours(int $hours) {
+    $this->seconds += $hours * 60 * 60;
+    return $this;
+  }
+
+  /**
+   * Recalculates the values
+   * 
+   * @return $this for a fluent interface
+   */
+  public function recalculate() {
+    $seconds = $this->toSeconds();
+    $this->d = floor($seconds / 60 / 60 / 24);
+    $seconds -= $this->d * 86400;
+    $this->h = floor($seconds / 60 / 60);
+    $seconds -= $this->h * 3600;
+    $this->i = floor($seconds / 60);
+    $seconds -= $this->i * 60;
+    $this->s = $seconds;
+    return $this;
+  }
+
+  public function toSeconds(): int {
+    return $this->seconds;
+  }
+
+  public function toMinutes(): float {
+    return ($this->toSeconds() / 60);
+  }
+
+  public function toHours(): float {
+    return ($this->toMinutes() / 60);
+  }
+
+  public function toDays(): float {
+    return ($this->toSeconds() / 86400);
+  }
+
+  public function compareTo($interval): int {
+    $i = Intervals::create($interval);
+    return $this->toSeconds() <=> $i->toSeconds();
+  }
+
+  /**
+   * 
+   * 
+   * @param  string $time
+   * @return Interval new instance
+   */
+  public static function createFromDateString($time) {
+    $di = parent::createFromDateString($time);
+    return Intervals::FromDateInterval($di);
+  }
+
+  /**
+   * Constructor
+   * 
+   * @param string $interval an interval specification.
+   */
+  public function fromTime(int $hours, int $minutes, int $seconds) {
+    if (Strings::startsWith($interval, '-')) {
+      $str = Strings::trimLeft($interval, '-');
+      parent::__construct($str);
+      $this->invert = true;
+    } else {
+      parent::__construct($interval);
+    }
+  }
+
+  public static function fromString(string $time): Duration {
+    $parts = explode(':', $time);
+    if (count($parts) < 3) {
+      array_unshift($parts, 0);
+    }
+    $dateint = 'PT' . $parts[0] . 'H' . $parts[1] . 'M' . $parts[2] . "S";
+    //echo "$dateint\n";
+    return new Duration($parts[2]);
+  }
+
+}
---
+++ tests/sphp/DateTime/DurationTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * SPHPlayground Framework (http://playgound.samiholck.com/)
+ *
+ * @link      https://github.com/samhol/SPHP-framework for the source repository
+ * @copyright Copyright (c) 2007-2018 Sami Holck <sami.holck@gmail.com>
+ * @license   https://opensource.org/licenses/MIT The MIT License
+ */
+
+namespace Sphp\Tests\DateTime;
+
+use PHPUnit\Framework\TestCase;
+use Sphp\DateTime\Duration;
+use Sphp\DateTime\Interval;
+
+/**
+ * Description of DurationTest
+ *
+ * @author  Sami Holck <sami.holck@gmail.com>
+ * @license https://opensource.org/licenses/MIT The MIT License
+ * @filesource
+ */
+class DurationTest extends TestCase{
+  
+  public function testdateString() {
+    $interval = Duration::fromString('1:00:00');
+    $this->assertSame(1.0, $interval->toHours());
+  }
+
+  public function intervalStrings(): array {
+    $strings[] = ['P0Y0M2DT1H22M1S', 'P2DT1H22M1S'];
+    $strings[] = ['P0Y1M0DT0H0M0S', 'P1M'];
+    $strings[] = ['P0Y0M0DT0H3M0S', 'PT3M'];
+    return $strings;
+  }
+
+  /**
+   * @dataProvider intervalStrings
+   */
+  public function testToString(string $interval_spec, string $expected) {
+    $interval = new Duration($interval_spec);
+    $this->assertSame($expected, "$interval");
+  }
+
+  public function testArithmetics() {
+    $interval = new Interval('P1DT1H');
+    $sum = $interval->add(new Interval('P1M1DT2H3M'));
+    $this->assertSame('P1M2DT3H3M', "$sum");
+    $div = $interval->add(new Interval('-P1M1DT2H3M'));
+    $this->assertSame('P1DT1H', "$div");
+    
+  }
+}
