46383cd0348580a41f70b20cb86f02ec24203dda
samhol <sami.holck@gmail.com>
2019-02-11 14:48:45 +0200

    Updates

--- tests/sphp/Html/Attributes/AttributeGeneratorTest.php
+++ tests/sphp/Html/Attributes/AttributeGeneratorTest.php
@@ -7,27 +7,6 @@ use Sphp\Exceptions\InvalidArgumentException;
 class AttributeGeneratorTest extends \PHPUnit\Framework\TestCase {
 
   /**
-   * @var AttributeGenerator 
-   */
-  protected $gen;
-
-  /**
-   * Sets up the fixture, for example, opens a network connection.
-   * This method is called before a test is executed.
-   */
-  protected function setUp() {
-    $this->gen = new AttributeGenerator();
-  }
-
-  /**
-   * Tears down the fixture, for example, closes a network connection.
-   * This method is called after a test is executed.
-   */
-  protected function tearDown() {
-    $this->gen = null;
-  }
-
-  /**
    * @return string[]
    */
   public function emptyData(): array {
@@ -44,27 +23,29 @@ class AttributeGeneratorTest extends \PHPUnit\Framework\TestCase {
   }
 
   public function testTypeMapping() {
-    $this->assertFalse($this->gen->isMapped('class'));
-    $this->gen->mapType('class', ClassAttribute::class);
-    $this->assertTrue($this->gen->isMapped('class'));
-    $object = $this->gen->createObject('class');
+    $gen = new AttributeGenerator();
+    $this->assertFalse($gen->isMapped('class'));
+    $gen->mapType('class', ClassAttribute::class);
+    $this->assertTrue($gen->isMapped('class'));
+    $object = $gen->createObject('class');
     $this->assertInstanceOf(ClassAttribute::class, $object);
     $this->expectException(InvalidArgumentException::class);
-    $this->gen->mapType('class', GeneralAttribute::class);
+    $gen->mapType('class', GeneralAttribute::class);
   }
 
   public function testSubTyping() {
-    $this->assertFalse($this->gen->isMapped('foo'));
-    $this->gen->mapType('foo', PatternAttribute::class, '/^foobar*$/');
-    $this->assertTrue($this->gen->isMapped('foo'));
-    $object = $this->gen->createObject('foo');
+    $gen = new AttributeGenerator();
+    $this->assertFalse($gen->isMapped('foo'));
+    $gen->mapType('foo', PatternAttribute::class, '/^foobar*$/');
+    $this->assertTrue($gen->isMapped('foo'));
+    $object = $gen->createObject('foo');
     $this->assertInstanceOf(PatternAttribute::class, $object);
-    $this->gen->mapType('foo', IdAttribute::class);
-    $object1 = $this->gen->createObject('foo');
+    $gen->mapType('foo', IdAttribute::class);
+    $object1 = $gen->createObject('foo');
     $this->assertInstanceOf(PatternAttribute::class, $object1);
     $this->assertInstanceOf(IdAttribute::class, $object1);
     $this->expectException(InvalidArgumentException::class);
-    $this->gen->mapType('foo', GeneralAttribute::class);
+    $gen->mapType('foo', GeneralAttribute::class);
   }
 
 }
