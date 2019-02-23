9dbcdc1b8b185d90bc872d6023158b2ff22f4ad2
Sami Holck <sami.holck@gmail.com>
2019-02-23 03:33:46 +0200

    Updates

--- manual/pages/props.php
+++ manual/pages/props.php
@@ -1,64 +1,100 @@
+<div class="card-info success">
+  <div class="card-info-label">
+    <div class="card-info-label-text">
+      <i class="fas fa-info-circle"></i>
+    </div>
+  </div>
+  <div class="card-info-content">
+    <h3 class="lead"> Mad Max: Fury Road</h3>
+    <p>In a post-apocalyptic world, in which people fight to the death, Max teams up with a mysterious woman, Furiousa, to try and survive.</p>
+  </div>
+</div>
 
-<pre>
-  <?php
-  $date = new DateTimeImmutable('last monday');
-  $start = $date->format('2018-09-10');
-  echo $start;
-  $query = <<<JSON
-query {
-  repository(owner: "samhol", name: "SPHP-framework") {
-    ref(qualifiedName: "master") {
-      target {
-        ... on Commit {
-          history(since: "{$start}T00:00:00Z" until:"{$start}T23:59:59Z") {
-            pageInfo {
-              hasNextPage
-              endCursor
-            }
-            edges {
-              node {
-                url
-                messageHeadline
-                pushedDate
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
+<div class="card-info primary">
+  <div class="card-info-label">
+    <div class="card-info-label-text">
+      Blue
+    </div>
+  </div>
+  <div class="card-info-content">
+    <h3 class="lead">Star Wars: Episode VII - The Force Awakens</h3>
+    <p>A continuation of the saga created by George Lucas set thirty years after Star Wars: Episode VI - Return of the Jedi.</p>
+  </div>
+</div>
 
-JSON;
-  $variables = '';
+<div class="card-info secondary">
+  <div class="card-info-label">
+    <div class="card-info-label-text">
+      Deal
+    </div>
+  </div>
+  <div class="card-info-content">
+    <h3 class="lead">Terminator Genisys</h3>
+    <p>After finding himself in a new time-line, Kyle Reese teams up with John Connor's mother Sarah and an aging terminator to try and stop the one thing that the future fears, "Judgement Day".</p>
+  </div>
+</div>
+
+<div class="card-info info">
+  <div class="card-info-label">
+    <div class="card-info-label-text">
+      FYI
+    </div>
+  </div>
+  <div class="card-info-content">
+    <h3 class="lead">Chappie</h3>
+    <p>In the near future, crime is patrolled by a mechanized police force. When one police droid, Chappie, is stolen and given new programming, he becomes the first robot with the ability to think and feel for himself.</p>
+  </div>
+</div>
+
+<div class="card-info alert">
+  <div class="card-info-label">
+    <div class="card-info-label-text">
+      Stop
+    </div>
+  </div>
+  <div class="card-info-content">
+    <h3 class="lead"> Poltergeist</h3>
+    <p>A family's suburban home is invaded by evil forces.
+    </p>
+  </div>
+</div>
+
+<div class="card-info warning">
+  <div class="card-info-label">
+    <div class="card-info-label-text">
+      Hey!
+    </div>
+  </div>
+  <div class="card-info-content">
+    <h3 class="lead">Jurassic World</h3>
+    <p>Twenty-two years after the events of Jurassic Park, Isla Nublar now features a fully functioning dinosaur theme park, Jurassic World, as originally envisioned by John Hammond. After 10 years of operation and visitor rates declining, in order to fulfill a corporate mandate, a new attraction is created to re-spark visitor's interest, which backfires horribly.
+    </p>
+  </div>
+</div>
+
+<div class="card-info">
+  <div class="card-info-label">
+    <div class="card-info-label-text">
+      Yo!
+    </div>
+  </div>
+  <div class="card-info-content">
+    <h3 class="lead"> Avengers: Age of Ultron </h3>
+    <p>When Tony Stark tries to jumpstart a dormant peacekeeping program, things go awry and it is up to the Avengers to stop the villainous Ultron from enacting his terrible plans.</p>
+  </div>
+</div>
+
+<div class="card-info alert">
+  <div class="card-info-label">
+    <div class="card-info-label-text">
+      Sale
+    </div>
+  </div>
+  <div class="card-info-content">
+    <h3 class="lead"> Death Star</h3>
+    <img src="https://img2.wikia.nocookie.net/__cb20090309234126/starwars/images/e/ee/DeathStar2.jpg" alt="death star" />
+    <p>The Death Star was a moon-sized Imperial military battlestation armed with a planet-destroying superlaser.</p>
+  </div>
+</div>
 
 
-
-  $json = json_encode(['query' => $query, 'variables' => $variables]);
-
-  $chObj = curl_init();
-  curl_setopt($chObj, CURLOPT_URL, 'https://api.github.com/graphql');
-  curl_setopt($chObj, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($chObj, CURLOPT_CUSTOMREQUEST, 'POST');
-//curl_setopt($chObj, CURLOPT_HEADER, true);
-  curl_setopt($chObj, CURLOPT_POSTFIELDS, $json);
-  curl_setopt($chObj, CURLOPT_HTTPHEADER, [
-      'User-Agent: PHP Script',
-      'Content-Type: application/json;charset=utf-8',
-      'Authorization: bearer 1569726f0d1efed0390c'
-      //'Authorization: bearer a0461769551af53d17e8167dcaf9b9d6ed43ce93'
-          ]
-  );
-
-  $response = curl_exec($chObj);
-  curl_close($chObj);
-  //var_dump($response);
-  $arr = json_decode($response, true);
-  foreach ($arr["data"]["repository"]["ref"]["target"]["history"]["edges"] as $node) {
-    //var_dump($node["node"]);
-    echo new Sphp\Html\Navigation\Hyperlink($node["node"]["url"], $node["node"]["messageHeadline"] . " at " . $node["node"]["pushedDate"]);
-  }
-  var_dump($arr);
-  var_dump(isset($arr["data"]["repository"]["ref"]["target"]["his1tory"]["edges"]));
-  ?>
-</pre>
