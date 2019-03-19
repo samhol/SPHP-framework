8bb51030e8dbcd28152a6ba38304c0ddc09428dc
samhol <sami.holck@gmail.com>
2019-03-19 15:32:19 +0200

    Updates

--- sphp/php/Sphp/Html/AbstractContainerTag.php
+++ sphp/php/Sphp/Html/AbstractContainerTag.php
@@ -33,10 +33,67 @@ use Traversable;
  * @link    https://github.com/samhol/SPHP-framework GitHub repository
  * @filesource
  */
-class AbstractContainerTag extends AbstractContainerComponent implements IteratorAggregate, ContainerComponent, ContentParser {
+class AbstractContainerTag extends AbstractComponent implements IteratorAggregate, ContainerComponent, ContentParser {
 
   use ContentParserTrait,
       TraversableTrait;
+
+  /**
+   * the inner content container
+   *
+   * @var Container
+   */
+  private $content;
+
+  /**
+   * Constructor
+   *
+   * @param  string $tagname the name of the tag
+   * @param  HtmlAttributeManager|null $attrManager the attribute manager of the component
+   * @param  Container|null $contentContainer the inner content container of the component
+   */
+  public function __construct(string $tagname, HtmlAttributeManager $attrManager = null, Container $contentContainer = null) {
+    parent::__construct($tagname, $attrManager);
+    $this->setInnerContainer($contentContainer);
+  }
+
+  public function __destruct() {
+    unset($this->content);
+    parent::__destruct();
+  }
+
+  public function __clone() {
+    $this->content = clone $this->content;
+    parent::__clone();
+  }
+
+  /**
+   * Sets the inner content container of the component
+   *
+   * @param  Container $contentContainer the inner content container of the component
+   * @return $this for a fluent interface
+   */
+  protected function setInnerContainer(Container $contentContainer = null) {
+    if (!($contentContainer instanceof Container)) {
+      $this->content = new PlainContainer();
+    } else {
+      $this->content = $contentContainer;
+    }
+    return $this;
+  }
+
+  /**
+   * Returns the content container or an element pointed by an optional index
+   *
+   * @return Container the content container
+   */
+  protected function getInnerContainer(): Container {
+    return $this->content;
+  }
+
+  public function contentToString(): string {
+    return $this->content->getHtml();
+  }
 
   public function append(...$content) {
     foreach ($content as $item) {
