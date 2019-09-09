3e13dff25a3f3517cb25a75a05b2c8caa960d609
samhol <sami.holck@gmail.com>
2019-09-09 19:55:08 +0300

    Update info.php

--- manual/snippets/icons/devicon/info.php
+++ manual/snippets/icons/devicon/info.php
@@ -6,6 +6,7 @@ require_once('/home/int48291/public_html/playground/manual/settings.php');
 
 use Sphp\Manual\Apps\Icons\DataFactory;
 use Sphp\Manual\Apps\Icons\Views\IconGroupInfoViewBuilder;
+
 if (!filter_has_var(INPUT_GET, 'iconSet')) {
   echo 'IconSet was not given!';
 }
@@ -17,6 +18,7 @@ if (filter_has_var(INPUT_GET, 'name')) {
     echo 'Icongroup ' . $name . ' was not found!';
   } else {
     $view = new \Sphp\Manual\Apps\Icons\Views\InfoVievs();
+    $view->associate('Font Awesome', new IconGroupInfoViewBuilder());
     echo $view->createHtmlFor($iconData);
   }
 } else {
